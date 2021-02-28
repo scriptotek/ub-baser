20434c8da392155dfa9a45f5eea1d37422ebaa17
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2021-02-28 23:24:37 +0100

    Add Sakbib [WIP]

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\ImportBibliomanuelCommand::class,
         \App\Console\Commands\ImportBibsysCommand::class,
         \App\Console\Commands\ImportOpesCommand::class,
+        \App\Console\Commands\ImportSakbibCommand::class,
         \App\Console\Commands\PurgeLogs::class,
         \App\Console\Commands\Deployed::class,
         \App\Console\Commands\PurgeLocks::class,
